7ad6948c237ecf1a50eb76913fa2d6e85e4a32c8
42trustokerezi <trustokerezi42@gmail.com>
2021-07-29 15:51:56 +0100

    complete CRUD for bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,24 +1,23 @@
 const express = require('express');
-
 const {
-    getBootcamps,
-    getBootcamp,
-    createBootcamp,
-    updateBootcamp,
-    deleteBootcamp
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp
 } = require('../controllers/bootcamps');
 
 const router = express.Router();
 
 router
-    .route('/')
-    .get(getBootcamps)
-    .post(createBootcamp);
+  .route('/')
+  .get(getBootcamps)
+  .post(createBootcamp);
 
 router
-    .route('/:id')
-    .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp);
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
