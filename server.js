3d56427b9486769196c48c8013028dc99c15fba8
42trustokerezi <trustokerezi42@gmail.com>
2021-08-03 00:42:10 +0100

    image upload for a bootcamp

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -20,10 +22,16 @@ const app = express();
 // Body parser
 app.use(express.json());
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// File uploading
+app.use(fileupload());
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
